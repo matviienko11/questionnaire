ddb1572073436fd15cff6605c5e50eff27b427f0
Bogdan Matvienko <bohdan.matvienko@faceit-team.com>
2021-08-06 16:06:42 +0300

    Created pages + implemented routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {QuestionManagementComponent} from "./pages/question-management/question-management.component";
+import {CreateQuestionComponent} from "./pages/create-question/create-question.component";
+import {QuestionEditComponent} from "./pages/question-edit/question-edit.component";
+import {QuestionListComponent} from "./pages/question-list/question-list.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'management',
+    component: QuestionManagementComponent
+  },
+  {
+    path: 'create',
+    component: CreateQuestionComponent
+  },
+  {
+    path: 'edit',
+    component: QuestionEditComponent
+  },
+  {
+    path: 'list',
+    component: QuestionListComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { QuestionManagementComponent } from './pages/question-management/question-management.component';
+import { CreateQuestionComponent } from './pages/create-question/create-question.component';
+import { QuestionEditComponent } from './pages/question-edit/question-edit.component';
+import { QuestionListComponent } from './pages/question-list/question-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuestionManagementComponent,
+    CreateQuestionComponent,
+    QuestionEditComponent,
+    QuestionListComponent
   ],
   imports: [
     BrowserModule,
