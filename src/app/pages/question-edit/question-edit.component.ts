fd5243bfe6e126c6bfb38d1a9596b6f756220dbb
Bogdan Matvienko <bohdan.matvienko@faceit-team.com>
2021-08-09 13:22:40 +0300

    added redirect button

--- src/app/pages/question-edit/question-edit.component.ts
+++ src/app/pages/question-edit/question-edit.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-question-edit',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuestionEditComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  toManagement() {
+    this.router.navigate(['/management'])
   }
 
 }
